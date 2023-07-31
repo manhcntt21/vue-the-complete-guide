be8c0a65a960282c6566bbf219215d21e0924482
Do Truong Manh <manhcntt2.1@gmail.com>
2023-07-31 21:41:10 +0700

    section4: game over screen

--- section4/prj-monster-01-starting-setup/app.js
+++ section4/prj-monster-01-starting-setup/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
+            winner: null,
         };
     },
     computed: {
@@ -67,6 +68,28 @@ const app = Vue.createApp({
 
             // monster attack after player heal
             this.attackPlayer();
+        },
+    },
+
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                // draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                // player lost
+                this.winner = 'monster';
+            }
+        },
+
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                // draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                // monster lost
+                this.winner = 'player';
+            }
         },
     },
 });
