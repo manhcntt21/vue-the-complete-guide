ffe60ee37d22116f203eed4a7106595f4633498f
Do Truong Manh <manhcntt2.1@gmail.com>
2023-07-31 20:24:02 +0700

    section4: attack action

---
+++ section4/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,27 @@
+function getRandomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        };
+    },
+
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomNumber(12, 5);
+            this.monsterHealth -= attackValue;
+
+            // call attackPlayer
+            this.attackPlayer();
+        },
+
+        attackPlayer() {
+            const attackValue = getRandomNumber(15, 8);
+            this.playerHealth -= attackValue;
+        },
+    },
+});
+app.mount('#game');
