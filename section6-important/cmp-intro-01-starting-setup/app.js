5f36838bdbb7fb08b93f780b3a802d8e3fc11754
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-01 14:10:38 +0700

    section6: split component

---
+++ section6-important/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,60 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manuel@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com',
+                },
+            ],
+        };
+    },
+    computed: {
+        description() {
+            return this.detailVisible ? 'Hide' : 'Show';
+        },
+    },
+    methods: {},
+});
+
+// create new a component
+app.component('friend-contact', {
+    template: `
+        <li>
+            <h2>{{ friend.name }}</h2>
+            <button @click="toggleDetail()">{{ description }} Details</button>
+            <ul v-if="detailVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+            </ul>
+        </li>    
+      `,
+
+    data() {
+        return {
+            detailVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com',
+            },
+        };
+    },
+
+    methods: {
+        toggleDetail() {
+            this.detailVisible = !this.detailVisible;
+        },
+    },
+});
+
+app.mount('#app');
