9a0e29513355184e37df5c58717b79ec084c006d
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-01 23:18:42 +0700

    section7: basic structure vue and workflow

---
+++ section7-important/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>My friends</h1>
+    </header>
+    <ul>
+        <!-- <li v-for="friend in friends" :key="friend.id">{{ friend }}</li> -->
+        <friend-contact
+            v-for="friend in friends"
+            :key="friend.id"
+            :friend="friend"
+        ></friend-contact>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manuel@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com',
+                },
+            ],
+        };
+    },
+    methods: {},
+    computed: {},
+};
+</script>
+
+<style></style>
---
+++ section7-important/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetail()">{{ description }} Details</button>
+        <ul v-if="detailVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['friend'],
+
+    data() {
+        return {
+            detailVisible: false,
+        };
+    },
+    computed: {
+        description() {
+            return this.detailVisible ? 'Hide' : 'Show';
+        },
+    },
+    methods: {
+        toggleDetail() {
+            this.detailVisible = !this.detailVisible;
+        },
+    },
+};
+</script>
