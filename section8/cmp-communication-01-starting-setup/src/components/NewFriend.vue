11ca7cbb75c1e3c72776a8acd05be3af6e1e8b46
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-02 19:51:15 +0700

    section8: add contact functionality

---
+++ section8/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label>name: </label>
+            <input type="text" v-model="friend.enteredName" />
+        </div>
+        <div>
+            <label>phone: </label>
+            <input type="text" v-model="friend.enteredPhone" />
+        </div>
+        <div>
+            <label>email: </label>
+            <input type="text" v-model="friend.enteredEmail" />
+        </div>
+        <div>
+            <button type="submit">Add Contact</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data() {
+        return {
+            friend: {
+                enteredName: '',
+                enteredPhone: '',
+                enteredEmail: '',
+            },
+        };
+    },
+
+    methods: {
+        submitData() {
+            this.$emit('add-contact', this.friend);
+        },
+    },
+};
+</script>
--- section8/cmp-communication-01-starting-setup/src/main.js
+++ section8/cmp-communication-01-starting-setup/src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue';
 import FriendContact from './components/FriendContact.vue';
 import BaseButton from './components/BaseButton.vue';
 import UserData from './components/UserData.vue';
+import NewFriend from './components/NewFriend.vue';
 
 const app = createApp(App);
 
 app.component('friend-contact', FriendContact);
 app.component('base-button', BaseButton);
 app.component('user-data', UserData);
+app.component('new-friend', NewFriend);
 
 app.mount('#app');
