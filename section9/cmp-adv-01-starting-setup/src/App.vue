cdee636e689c352121f33f894acf5d74f27ea967
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-22 03:49:45 +0700

    section9: scoped slot

--- section9/cmp-adv-01-starting-setup/src/App.vue
+++ section9/cmp-adv-01-starting-setup/src/App.vue
@@ -1,44 +1,56 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+    <div>
+        <the-header></the-header>
+        <badge-list></badge-list>
+        <user-info
+            :full-name="activeUser.name"
+            :info-text="activeUser.description"
+            :role="activeUser.role"
+        ></user-info>
+        <!-- <course-goal>
+            <template #default="slotProps">
+                <h2>{{ slotProps.item }}</h2>
+                <p>{{ slotProps.anotherProp }}</p>
+            </template>
+        </course-goal> -->
+        <course-goal #default="slotProps">
+            <h2>{{ slotProps.item }}</h2>
+            <p>{{ slotProps.anotherProp }}</p>
+        </course-goal>
+    </div>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoal from './components/CourseGoal.vue';
 
 export default {
-  components: {
-    "the-header": TheHeader,
-    BadgeList,
-    UserInfo,
-  },
-  data() {
-    return {
-      activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
-      },
-    };
-  },
+    components: {
+        'the-header': TheHeader,
+        BadgeList,
+        UserInfo,
+        CourseGoal,
+    },
+    data() {
+        return {
+            activeUser: {
+                name: 'Maximilian Schwarzmüller',
+                description: 'Site owner and admin',
+                role: 'admin',
+            },
+        };
+    },
 };
 </script>
 
 <style>
 html {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+    margin: 0;
 }
 </style>
