93aa4861fe619d100e35f4d71515faec723a4f3c
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-21 23:56:46 +0700

    section9: declare global & local components

--- section9/cmp-adv-01-starting-setup/src/App.vue
+++ section9/cmp-adv-01-starting-setup/src/App.vue
@@ -11,13 +11,22 @@
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  components: {
+    "the-header": TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
--- section9/cmp-adv-01-starting-setup/src/main.js
+++ section9/cmp-adv-01-starting-setup/src/main.js
@@ -1,16 +1,13 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
-import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import App from "./App.vue";
+
+import BaseBadge from "./components/BaseBadge.vue";
 
 const app = createApp(App);
 
-app.component('the-header', TheHeader);
-app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
+// global components
 
-app.mount('#app');
+app.component("base-badge", BaseBadge);
+
+app.mount("#app");
