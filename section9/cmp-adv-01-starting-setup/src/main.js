ff2c9212fc3b50b1ebf85f62fae1cd4840f28517
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-22 00:42:21 +0700

    section9: scoped style & slot

--- section9/cmp-adv-01-starting-setup/src/main.js
+++ section9/cmp-adv-01-starting-setup/src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from "vue";
+import { createApp } from 'vue';
 
-import App from "./App.vue";
+import App from './App.vue';
 
-import BaseBadge from "./components/BaseBadge.vue";
+import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 
 // global components
 
-app.component("base-badge", BaseBadge);
-
-app.mount("#app");
+app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
+app.mount('#app');
