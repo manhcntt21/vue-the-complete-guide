33645224d49550b31a016ad77268aff912088183
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-03 11:19:37 +0700

    section14: animation basics with css transition

--- section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
+++ section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +10,15 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+    };
   },
   methods: {
     showDialog() {
@@ -23,6 +26,10 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+
+    animateBlock() {
+      this.animatedBlock = true;
     },
   },
 };
@@ -57,6 +64,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +76,9 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  transform: translateX(-150px);
 }
 </style>
