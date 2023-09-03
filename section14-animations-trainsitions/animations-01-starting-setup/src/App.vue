d42c1d1634228b642c3bb8d0ea2e8aa64da31cb0
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-03 15:20:25 +0700

    section14: exception: multiple child in transitio component

--- section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
+++ section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
@@ -9,6 +9,12 @@
     </Transition>
     <button @click="tagggleParagraph">Toggle Button</button>
   </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUser" v-if="!usersAreVisible">Show users</button>
+      <button @click="hideUser" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -25,9 +31,17 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    showUser() {
+      this.usersAreVisible = true;
+    },
+    hideUser() {
+      this.usersAreVisible = false;
+    },
+
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -132,5 +146,20 @@ button:active {
 .para-leave-to {
   /* opacity: 0;
   transform: translateY(-30px); */
+}
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 </style>
