427ea2113f116a1b84527cb68bd5a44a7a535f00
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-03 12:46:39 +0700

    section14: css animation with transition component

--- section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
+++ section14-animations-trainsitions/animations-01-starting-setup/src/App.vue
@@ -109,26 +109,28 @@ button:active {
 }
 
 .v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
 .v-enter-active {
-  transition: all 0.3s ease-out;
+  /* transition: all 0.3s ease-out; */
+  animation: slide-fade 0.3s ease-out;
 }
 .v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
 .v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 .v-leave-active {
-  transition: all 0.3s ease-out;
+  /* transition: all 0.3s ease-out; */
+  animation: slide-fade 0.3s ease-out;
 }
 .v-leave-to {
-  opacity: 0;
-  transform: translateY(-30px);
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
 </style>
