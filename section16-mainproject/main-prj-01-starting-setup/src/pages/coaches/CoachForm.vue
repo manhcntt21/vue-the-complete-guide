3be013d4df82f44c93b5927460dd07e6e5636f75
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-06 21:48:03 +0700

    form validate

--- section16-mainproject/main-prj-01-starting-setup/src/pages/coaches/CoachForm.vue
+++ section16-mainproject/main-prj-01-starting-setup/src/pages/coaches/CoachForm.vue
@@ -1,36 +1,81 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-controls">
+    <div class="form-controls" :class="{ invalid: !firstName.isValid }">
       <label for="firstname">FirstName</label>
-      <input type="text" id="firstname" v-model.trim="firstName" />
+      <input
+        type="text"
+        id="firstname"
+        v-model.trim="firstName.val"
+        @blur="clearValidity('firstName')"
+      />
+      <p v-if="!firstName.isValid">Firstname must not be empty.</p>
     </div>
-    <div class="form-controls">
+
+    <div class="form-controls" :class="{ invalid: !lastName.isValid }">
       <label for="lastname">LastName</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <input
+        type="text"
+        id="lastname"
+        v-model.trim="lastName.val"
+        @blur="clearValidity('lastName')"
+      />
+      <p v-if="!lastName.isValid">Lastname must not be empty.</p>
     </div>
-    <div class="form-controls">
+    <div class="form-controls" :class="{ invalid: !description.isValid }">
       <label for="description">Description</label>
-      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+      <textarea
+        id="description"
+        rows="5"
+        v-model.trim="description.val"
+        @blur="clearValidity('description')"
+      ></textarea>
+      <p v-if="!description.isValid">Description must not be empty.</p>
     </div>
-    <div class="form-controls">
+    <div class="form-controls" :class="{ invalid: !rate.isValid }">
       <label for="rate">HourlyRate</label>
-      <input type="number" id="rate" v-model.number="rate" />
+      <input
+        type="number"
+        id="rate"
+        v-model.number="rate.val"
+        @blur="clearValidity('rate')"
+      />
+      <p v-if="!rate.isValid">Rate must not be greater than 0.</p>
     </div>
-    <div class="form-controls">
+    <div class="form-controls" :class="{ invalid: !areas.isValid }">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" v-model="areas" value="frontend" />
+        <input
+          type="checkbox"
+          id="frontend"
+          v-model="areas.val"
+          value="frontend"
+          @blur="clearValidity('areas')"
+        />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" v-model="areas" value="backend" />
+        <input
+          type="checkbox"
+          id="backend"
+          v-model="areas.val"
+          value="backend"
+          @blur="clearValidity('areas')"
+        />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" v-model="areas" value="career" />
+        <input
+          type="checkbox"
+          id="career"
+          v-model="areas.val"
+          value="career"
+          @blur="clearValidity('areas')"
+        />
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!areas.isValid">At least one expertise muse be selected.</p>
     </div>
+    <p v-if="!formIsValid">Please fix the above errors and submit again</p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -42,24 +87,59 @@ export default {
   emits: ['save-data'],
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      description: '',
-      rate: null,
-      areas: [],
+      firstName: { val: '', isValid: true },
+      lastName: { val: '', isValid: true },
+      description: { val: '', isValid: true },
+      rate: { val: null, isValid: true },
+      areas: { val: [], isValid: true },
+      formIsValid: true,
     };
   },
   computed: {
     ...mapGetters('coaches', ['coaches']),
   },
   methods: {
+    clearValidity(input) {
+      console.log('a');
+      this[input].isValid = true;
+    },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
     submitForm() {
+      this.validateForm();
+      // dung chuong trinh neu co truong nao khong hop le
+      if (!this.formIsValid) {
+        return;
+      }
       const formData = {
-        first: this.firstName,
-        last: this.lastName,
-        desc: this.description,
-        rate: this.rate,
-        areas: this.areas,
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val,
       };
 
       console.log(formData);
