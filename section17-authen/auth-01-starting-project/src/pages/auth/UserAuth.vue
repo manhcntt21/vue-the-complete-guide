db3de77fcb9454ba4b3ac15f90008194c3d0f516
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-08 15:43:09 +0700

    section 17: login

--- section17-authen/auth-01-starting-project/src/pages/auth/UserAuth.vue
+++ section17-authen/auth-01-starting-project/src/pages/auth/UserAuth.vue
@@ -70,15 +70,16 @@ export default {
         return;
       }
       // send http request...
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
       this.isLoading = true;
       try {
         if (this.mode === 'login') {
-          //...
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
       } catch (error) {
         this.error =
--- section17-authen/auth-01-starting-project/src/store/modules/auth/actions.js
+++ section17-authen/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -1,6 +1,33 @@
 export default {
-  login(context) {
-    console.log(context);
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAZThDU0D4AAs5yonf5HSvMKPYOsSlHncs',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = Error(
+        response.message || 'Failed to authenticate, check your login data.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
   },
 
   async signup(context, payload) {
