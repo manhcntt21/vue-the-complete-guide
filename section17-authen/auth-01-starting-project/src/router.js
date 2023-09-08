a46c416acc1c2b3cd403f9ab65d9d6a5cd80fcf4
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-08 14:49:03 +0700

    section 17: authentication page

--- section17-authen/auth-01-starting-project/src/router.js
+++ section17-authen/auth-01-starting-project/src/router.js
@@ -6,7 +6,7 @@ import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-
+import UserAuth from './pages/auth/UserAuth.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -17,13 +17,14 @@ const router = createRouter({
       component: CoachDetail,
       props: true,
       children: [
-        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
-      ]
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
     { path: '/register', component: CoachRegistation },
     { path: '/requests', component: RequestsReceived },
-    { path: '/:notFound(.*)', component: NotFound }
-  ]
+    { path: '/auth', component: UserAuth },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
 });
 
 export default router;
