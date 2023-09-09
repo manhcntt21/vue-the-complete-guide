1b98943195722e0ac7beb3d7af3040519080f26c
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-09 17:24:55 +0700

    section19: Composition API: assignment 7

---
+++ section19-composition-api/composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="data.show">{{ data.mainCourse }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggle">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+// const mainCourse = ref('Master Vuejs');
+// const show = ref(true);
+
+// const toggle = function () {
+//   show.value = !show.value;
+// };
+
+// const data = ref({
+//   mainCourse: 'Master Vuejs',
+//   show: true,
+// });
+
+// const toggle = function () {
+//   data.value.show = !data.value.show;
+// };
+
+const data = reactive({
+  mainCourse: 'Master Vuejs',
+  show: true,
+});
+
+const toggle = function () {
+  data.show = !data.show;
+};
+
+console.log(data);
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
