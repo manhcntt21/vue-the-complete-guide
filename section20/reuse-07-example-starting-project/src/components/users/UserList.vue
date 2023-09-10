2f3538a8c750669b39f2e77031522b9ef629a73d
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-10 23:00:43 +0700

    section 20: sort

--- section20/reuse-07-example-starting-project/src/components/users/UserList.vue
+++ section20/reuse-07-example-starting-project/src/components/users/UserList.vue
@@ -26,10 +26,11 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 
 import UserItem from './UserItem.vue';
 import useSearch from '../hooks/search.js';
+import useSort from '../hooks/sort.js';
 export default {
   components: {
     UserItem,
@@ -70,28 +71,31 @@ export default {
       'fullName'
     );
 
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
+    // const sorting = ref(null);
+    // const displayedUsers = computed(function () {
+    //   if (!sorting.value) {
+    //     return availableItems.value;
+    //   }
+    //   return availableItems.value.slice().sort((u1, u2) => {
+    //     if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+    //       return 1;
+    //     } else if (sorting.value === 'asc') {
+    //       return -1;
+    //     } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+    //       return -1;
+    //     } else {
+    //       return 1;
+    //     }
+    //   });
+    // });
 
-    function sort(mode) {
-      sorting.value = mode;
-    }
-
+    // function sort(mode) {
+    //   sorting.value = mode;
+    // }
+    const { displayedUsers, sort, sorting } = useSort(
+      availableItems,
+      'fullName'
+    );
     return {
       enteredSearchTerm,
       updateSearch,
